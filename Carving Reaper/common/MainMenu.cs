7735f6f0f49f0b18a617ac24b2f64d798fbba953
Philip <philipchell@hotmail.com>
2021-10-16 12:54:50 +0200

    added controller support and attack button

--- Carving Reaper/common/MainMenu.cs
+++ Carving Reaper/common/MainMenu.cs
@@ -3,9 +3,11 @@ using System;
 
 public class MainMenu : Control
 {
+    Button playButton;
     public override void _Ready()
     {
-
+        playButton = GetNode<Button>("PlayButton");
+        playButton.GrabFocus();
     }
 
     public void Play()
