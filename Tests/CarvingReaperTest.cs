2403caa8c2ad3ecb2f5c74d1199166ea483aaecc
Dennis Nikolay <dennisnikolay@posteo.de>
2021-10-16 11:40:16 +0200

    more tests

--- Tests/CarvingReaperTest.cs
+++ Tests/CarvingReaperTest.cs
@@ -9,10 +9,38 @@ namespace Tests
         [Test]
         public void TestMoveLeft()
         {
+            BasicMovementDirectionTest(Vector2.Left, new Vector2(-20, 0));
+        }
+
+        [Test]
+        public void TestMoveRight()
+        {
+            BasicMovementDirectionTest(Vector2.Right, new Vector2(20, 0));
+        }
+
+        [Test]
+        public void TestMoveUp()
+        {
+            BasicMovementDirectionTest(Vector2.Up, new Vector2(0, -20));
+        }
+
+        [Test]
+        public void TestMoveDown()
+        {
+            BasicMovementDirectionTest(Vector2.Down, new Vector2(0, 20));
+        }
+
+        [Test]
+        public void TestMoveDoesNotStopImmediately()
+        {
+
+        }        
+
+        protected void BasicMovementDirectionTest(Vector2 direction, Vector2 expectedVelocity){
             CarvingReaperMovementState carvingReaperMovementState = new CarvingReaperMovementState();
-            Vector2 velocity = carvingReaperMovementState.MoveByInput(1, Vector2.Left);
+            Vector2 velocity = carvingReaperMovementState.MoveByInput(1, direction);
             Assert.AreEqual(
-                new Vector2(-20, 0),
+                expectedVelocity,
                 velocity
             );
         }
